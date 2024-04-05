e5a82de66a9bdb9173fa016184fe77eef3ec78d9
Abylai Omar <abylaiomar@vl965-172-31-174-190.wireless.umass.edu>
2024-04-05 16:18:55 -0400

    Create base project, removed storyboard references

---
+++ FoodDeliveryApp/FoodDeliveryApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FoodDeliveryApp
+//
+//  Created by Abylai Omar on 4/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
